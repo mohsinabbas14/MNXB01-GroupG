dad2e9a9859e578a5af7b970c6dd8772c9fea0f1
FrankJohansson <nat13fjo@student.lu.se>
2018-10-31 15:46:19 +0100

    Reads files form path and creates a plot of the mean temperature (using a dummy function)

---
+++ tempTrender.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <vector>
+#include "tempTrender.h"
+// Authors: Frank Johansson & Mailk Mohsin 2018-10-30
+// ROOT library obejcts
+#include <TF1.h> // 1d function class
+#include <TH1.h> // 1d histogram classes
+#include <TStyle.h>  // style object
+#include <TMath.h>   // math functions
+#include <TCanvas.h> // canvas object
+#include <TRandom.h>
+#include <TGraph.h>
+#include <TLegend.h>
+
+tempTrender::tempTrender(string filePath) {
+	its_filePath = filePath;
+	cout << "The user supplied " << filePath << " as the path to the data file." << endl;
+	cout << "You should probably store this information in a member variable of the class. Good luck with the project! :)" << endl;
+}
+
+void tempTrender::readFile()
+{	
+	string helpString; // help variable
+	
+	// open input file
+	ifstream infile(its_filePath.c_str());
+	string fileHelpString = its_filePath.substr(its_filePath.find("_", 0), its_filePath.find(".", 0));
+	fileHelpString.erase(fileHelpString.find("csv"));
+	fileHelpString = string("mT" + fileHelpString + "dat");
+	ofstream outfile(fileHelpString.c_str());		
+	while(getline(infile,helpString)) {
+		
+		if (helpString == "Datum;Tid (UTC);Lufttemperatur;Kvalitet;;Tidsutsnitt:") {
+					break;
+		}	
+	}
+	
+	while(getline(infile,helpString)) {
+		int i = 0;
+		for( int j = 0; j < helpString.size(); j++) {
+			if( helpString.at(j) == ';') {
+				helpString.at(j) = ' ';
+				i++;
+			}
+			if(i > 3) break; 
+		}
+		
+		helpString = helpString.substr(0, helpString.find(";", 0));
+		
+		if(helpString.find('Y') != string::npos) {
+			continue;
+		}
+		string helpString1 = helpString.substr(0, helpString.find(" ", 0));
+		string helpString2 = helpString.substr(helpString.find(" ", 1), helpString.find(" ", 1));
+		string helpString3 = helpString.substr(helpString.find(" ", 11), size_t(12));
+		helpString3.erase(helpString3.find(" G"));
+		//cout << helpString1 << " " << helpString2 << " " << helpString3 << " " << endl;
+		
+		outfile << helpString1 << " " << helpString2 << " " << helpString3 << endl;
+	}
+}
+
+void tempTrender::tempPerDay() {
+	vector<double> temperatures = vectorFunction();
+	vector<double> days;
+	cout << temperatures.size() << endl;
+	for(size_t i = 0 ; i < temperatures.size(); i++) {
+		days.push_back(i);
+	}
+	cout << days.size() << endl;
+	
+	TCanvas *c1 = new TCanvas("c1","Mean Temperatures",500,20,1000,500);
+
+    c1->SetFillColor(42);
+    c1->SetGrid();
+    
+    double* daysA = &days[0];
+    double* temperaturesA = &temperatures[0];
+   
+    TGraph *gr = new TGraph(int(temperatures.size()),daysA,temperaturesA);
+    gr->SetLineColor(2);
+    gr->SetLineWidth(4);
+    gr->SetMarkerColor(4);
+    gr->SetMarkerStyle(21);
+    gr->SetTitle("Mean Temperature Per Day (all years)");
+    gr->GetXaxis()->SetTitle("Days");
+    gr->GetYaxis()->SetTitle("Temeratures");
+    gr->GetXaxis()->CenterTitle();
+    gr->GetYaxis()->CenterTitle();
+    gr->Draw("ACP");
+}
+
+vector<double> tempTrender::vectorFunction() {
+	readFile();
+	vector<double> temperatures;
+	double temp;
+	string discard;
+	ifstream infile("/home/courseuser/FrankJohanssonProject/MNXB01-project/code/mT_Lund.dat");
+	int i = 0;
+	while(infile >> discard) {
+		
+		
+		
+		infile >> discard;
+		infile >> temp;
+		//cout << temp << endl;
+		temperatures.push_back(temp);
+		
+		i++;
+		
+		//if(i > 20) break; 
+	}
+	return temperatures;
+}
